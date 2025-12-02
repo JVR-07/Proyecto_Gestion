2d4a70e66687d65a63c351ab9cd98520a8993952
JVR-07 <machado.jvr07@gmail.com>
2025-12-01 23:23:47 -0800

    Adding allowed hosts

--- SDC-Django/sdc/settings.py
+++ SDC-Django/sdc/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g!#etf=(had@qxkb)##bxct6m#fzu)&$r-is*z8&+#a#5)g%*0'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
